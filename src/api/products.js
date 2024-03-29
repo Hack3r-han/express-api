a0816cc1af0f6270df306cfd7f08c9d322f8cf5b
Hack3r-han <hannahphil27@gmail.com>
2024-03-29 20:15:40 +0100

    Change the language from English to Spanish for consistency

--- src/api/products.js
+++ src/api/products.js
@@ -3,132 +3,120 @@ const express = require('express');
 const router = express.Router();
     
 const productsData = [
+  [
     {
-      "id": 1,
-      "name": "Cowboy Boots",
-      "description": "Pair these outlaw boots with your favorite jeans, a worn-in hat, and a leather jacket for an ensemble that exudes Western charisma. Whether you're in a honky-tonk or blazing your own trail, these boots are your partner in crime. Size 9.",
+      "id_product": 1,
+      "name": "Botas Vaqueras",
+      "description": "Combina estas botas de forajido con tus vaqueros favoritos, un sombrero gastado y una chaqueta de cuero para un conjunto que exuda carisma occidental. Ya sea en un honky-tonk o abriendo tu propio camino, estas botas son tu cómplice. Talla 9.",
       "price": 35,
       "image": "https://img.freepik.com/free-photo/cowboy-inspiration-with-boots-chair_23-2149484732.jpg?w=826&t=st=1705568555~exp=1705569155~hmac=0b17fe38c210a44a615e6ca1529140efb1c3809681e7d1f7de7e1ae652c2544b",
-      "img": "https://img.freepik.com/free-photo/cowboy-inspiration-with-boots-chair_23-2149484732.jpg?w=826&t=st=1705568555~exp=1705569155~hmac=0b17fe38c210a44a615e6ca1529140efb1c3809681e7d1f7de7e1ae652c2544b",
-      "status": "used"
+      "status": "usado"
     },
     {
-      "id": 2,
-      "name": "Woolen Jumper",
-      "description": "Cozy and stylish woolen jumper to keep you warm during chilly seasons. This garment seamlessly combines comfort and fashion, making it a must-have addition to your winter wardrobe. Size S.",
+      "id_product": 2,
+      "name": "Suéter de Lana",
+      "description": "Suéter de lana acogedor y elegante para mantenerte abrigado durante las temporadas frías. Esta prenda combina perfectamente comodidad y moda, convirtiéndola en un elemento imprescindible en tu guardarropa de invierno. Talla S.",
       "price": 15,
       "image": "https://img.freepik.com/premium-photo/sweater-isolated-white_87394-14018.jpg?w=900",
-      "img": "https://img.freepik.com/premium-photo/sweater-isolated-white_87394-14018.jpg?w=900",
-      "status": "used"
+      "status": "usado"
     },
     {
-      "id": 3,
-      "name": "Watermelon Cooler",
-      "description": "Designed to keep the essence of summer always at your fingertips. This innovative fridge is not just a cooling appliance; it's a testament to convenience, ensuring that the crisp, refreshing taste of chilled watermelon is available whenever and wherever you desire.",
+      "id_product": 3,
+      "name": "Enfriador de Sandía",
+      "description": "Diseñado para mantener la esencia del verano siempre al alcance de tu mano. Este innovador refrigerador no es solo un electrodoméstico de enfriamiento; es un testimonio de conveniencia, asegurando que el sabor fresco y refrescante de la sandía fría esté disponible siempre que lo desees.",
       "price": 20,
       "image": "https://i.gzn.jp/img/2010/07/29/marugoto_tamachan/P1210050.jpg",
-      "img": "https://i.gzn.jp/img/2010/07/29/marugoto_tamachan/P1210050.jpg",
-      "status": "new"
+      "status": "nuevo"
     },
     {
-      "id": 4,
-      "name": "One-Sheet Wonder",
-      "description": "Introducing our revolutionary Quantum Absorbency Toilet Roll—an unparalleled breakthrough in bathroom innovation. With an extreme absorbing power that defies expectations, this toilet roll is engineered to redefine your bathroom experience.",
+      "id_product": 4,
+      "name": "Maravilla de una Sola Hoja",
+      "description": "Presentamos nuestro revolucionario Rollo de Papel Higiénico de Absorción Cuántica: un avance sin igual en la innovación del baño. Con un poder de absorción extrema que desafía las expectativas, este rollo de papel higiénico está diseñado para redefinir tu experiencia en el baño.",
       "price": 8,
       "image": "https://img.freepik.com/premium-photo/rolls-white-toilet-paper-textured-background_460171-3109.jpg?w=826",
-      "img": "https://img.freepik.com/premium-photo/rolls-white-toilet-paper-textured-background_460171-3109.jpg?w=826",
-      "status": "new"
+      "status": "nuevo"
     },
     {
-      "id": 5,
-      "name": "Knitted Jumper",
-      "description": "Knitted sweater made from high-quality 100% merino wool. Please note that the mushrooms are not edible. Size M.",
+      "id_product": 5,
+      "name": "Suéter de Punto",
+      "description": "Suéter de punto hecho de lana merina 100% de alta calidad. Ten en cuenta que los champiñones no son comestibles. Talla M.",
       "price": 20,
       "image": "https://img.freepik.com/premium-photo/baby-knitted-clothes-handmade-knitted-sweater-with-fly-agaric-mushrooms-jacket-with-embroidery_108930-7294.jpg?w=826",
-      "img": "https://img.freepik.com/premium-photo/baby-knitted-clothes-handmade-knitted-sweater-with-fly-agaric-mushrooms-jacket-with-embroidery_108930-7294.jpg?w=826",
-      "status": "used"
+      "status": "usado"
     },
     {
-      "id": 6,
-      "name": "Dali's Espadrilles",
-      "description": "Once graced by the surreal master himself, Salvador Dali. More than mere footwear, these espadrilles offer a unique portal into the fantastical landscapes that fueled the artist's creativity. The canvas of your mind will become a playground of surreal ideas.",
+      "id_product": 6,
+      "name": "Alpargatas de Dalí",
+      "description": "Una vez honradas por el propio maestro surrealista, Salvador Dalí. Más que meramente calzado, estas alpargatas ofrecen un portal único hacia los paisajes fantásticos que alimentaron la creatividad del artista. El lienzo de tu mente se convertirá en un patio de recreo de ideas surrealistas.",
       "price": 100,
       "image": "https://tonipons.com/media/catalog/product/cache/8a1ada7a5dd5cf4c25574b2c27fe03d6/1/6/1676392767_dali_negre__1.2209.jpg",
-      "img": "https://tonipons.com/media/catalog/product/cache/8a1ada7a5dd5cf4c25574b2c27fe03d6/1/6/1676392767_dali_negre__1.2209.jpg",
-      "status": "new"
+      "status": "nuevo"
     },
     {
-      "id": 7,
-      "name": "Wearable Umbrella",
-      "description": "Designed for those who refuse to let a sudden downpour dampen their style, this revolutionary accessory combines functionality with fashion.",
+      "id_product": 7,
+      "name": "Paraguas Vestible",
+      "description": "Diseñado para aquellos que se niegan a dejar que un repentino aguacero empañe su estilo, este accesorio revolucionario combina funcionalidad y moda.",
       "price": 10,
       "image": "https://m.media-amazon.com/images/I/71r86ZoozYL.jpg",
-      "img": "https://m.media-amazon.com/images/I/71r86ZoozYL.jpg",
-      "status": "new"
+      "status": "nuevo"
     },
     {
-      "id": 8,
-      "name": "Speaker",
-      "description": "Immerse yourself in a realm of unparalleled audio excellence with this amazing speaker. From deep, resonant bass to the finest trebles, experience audio clarity like never before.",
+      "id_product": 8,
+      "name": "Altavoz",
+      "description": "Sumérgete en un reino de excelencia auditiva sin igual con este asombroso altavoz. Desde graves profundos y resonantes hasta los mejores agudos, experimenta una claridad de audio como nunca antes.",
       "price": 30,
       "image": "https://img.freepik.com/free-photo/still-life-tech-device_23-2150722656.jpg?w=826&t=st=1705573130~exp=1705573730~hmac=1c34cc540be0a2af14a3bf4faab3aa88ac2ca5098d14164fd5770af3b781e20d",
-      "img": "https://img.freepik.com/free-photo/still-life-tech-device_23-2150722656.jpg?w=826&t=st=1705573130~exp=1705573730~hmac=1c34cc540be0a2af14a3bf4faab3aa88ac2ca5098d14164fd5770af3b781e20d",
-      "status": "used"
+      "status": "usado"
     },
     {
-      "id": 9,
-      "name": "Fried Confidence Socks",
-      "description": "Step confidently into a world of self-assured style with these socks. Forget about walking on eggshells and embrace every step with boldness. Crafted for those who exude confidence, these socks boast a fried egg design.",
+      "id_product": 9,
+      "name": "Calcetines de Confianza Frita",
+      "description": "Pisa con confianza en un mundo de estilo seguro con estos calcetines. Olvídate de caminar con cautela y abraza cada paso con audacia. Fabricados para aquellos que irradian confianza, estos calcetines presumen un diseño de huevo frito.",
       "price": 8,
       "image": "https://thelagaadi.com/cdn/shop/collections/long_socks-2x3_copy_982b4e38-6e3c-439c-b25a-9f1e48540a17.jpg?v=1695974128&width=1080",
-      "img": "https://thelagaadi.com/cdn/shop/collections/long_socks-2x3_copy_982b4e38-6e3c-439c-b25a-9f1e48540a17.jpg?v=1695974128&width=1080",
-      "status": "new"
+      "status": "nuevo"
     },
     {
-      "id": 10,
+      "id_product": 10,
       "name": "Koh-i-Noor",
-      "description": "Behold the Koh-i-Noor Diamond, a treasure that embodies a rich tapestry of history, power, and unparalleled beauty. This world-renowned diamond has a history steeped in mystery and grandeur.",
+      "description": "Contempla el Diamante Koh-i-Noor, un tesoro que encarna una rica tapicería de historia, poder y belleza sin igual. Este diamante de renombre mundial tiene una historia envuelta en misterio y grandeza.",
       "price": 100000000,
       "image": "https://englishtribuneimages.blob.core.windows.net/gallary-content/2023/4/2023_4$largeimg_486849653.jpg",
-      "img": "https://englishtribuneimages.blob.core.windows.net/gallary-content/2023/4/2023_4$largeimg_486849653.jpg",
-      "status": "new"
+      "status": "nuevo"
     },
     {
-      "id": 11,
-      "name": "The Golden Fleece",
-      "description": "An embodiment of the suns golden rays.  This shimmering fleece, once guarded by the mighty ram Chrysomallos, beckons those with the heart of a hero to embark on a journey of unparalleled significance.",
+      "id_product": 11,
+      "name": "El Vellocino de Oro",
+      "description": "Una encarnación de los rayos dorados del sol. Este brillante vellón, una vez guardado por el poderoso carnero Chrysomallos, llama a aquellos con el corazón de un héroe a embarcarse en un viaje de significado sin igual.",
       "price": 90,
       "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Jason_Pelias_Louvre_K127.jpg/1200px-Jason_Pelias_Louvre_K127.jpg",
-      "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Jason_Pelias_Louvre_K127.jpg/1200px-Jason_Pelias_Louvre_K127.jpg",
-      "status": "used"
+      "status": "usado"
     },
     {
-      "id": 12,
+      "id_product": 12,
       "name": "WC Fish n Flush",
       "price": 99.90,
-      "status": "new",
-      "description": "Limited edition, and in perfect condition. Does not include fish",
-      "image": "https://assets.newatlas.com/dims4/default/32dc20a/2147483647/strip/true/crop/530x647+0+0/resize/530x647!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2Farchive%2F6076_29080652716.jpg",
-      "img": "https://assets.newatlas.com/dims4/default/32dc20a/2147483647/strip/true/crop/530x647+0+0/resize/530x647!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2Farchive%2F6076_29080652716.jpg"
+      "status": "nuevo",
+      "description": "Edición limitada, y en perfectas condiciones. No incluye peces.",
+      "image": "https://assets.newatlas.com/dims4/default/32dc20a/2147483647/strip/true/crop/530x647+0+0/resize/530x647!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2Farchive%2F6076_29080652716.jpg"
     },
     {
-      "id": 13,
-      "name": "Cubed Watermelon",
+      "id_product": 13,
+      "name": "Sandía en Cubitos",
       "price": 29.99,
-      "status": "new",
-      "description": "Brought directly from the land of the rising sun, signed by Tomoyoki Ono. Includes moulds to make your own square watermelons.",
-      "image": "https://cdn.thisiswhyimbroke.com/images/cubed-watermelon-mold-640x533.jpg",
-      "img": "https://cdn.thisiswhyimbroke.com/images/cubed-watermelon-mold-640x533.jpg"
+      "status": "nuevo",
+      "description": "Traído directamente de la tierra del sol naciente, firmado por Tomoyoki Ono. Incluye moldes para hacer tus propias sandías.",
+      "image": "https://cdn.thisiswhyimbroke.com/images/cubed-watermelon-mold-640x533.jpg"
     },
     {
-      "id": 14,
+      "id_product": 14,
       "name": "Whine Glass",
       "price": 200,
-      "status": "new",
-      "description": "The design is so bad you'll want to whine. But at least there's zero spillage.",
-      "image": "https://www.yankodesign.com/images/design_news/2020/02/auto-draft/uncomfortable_designs-ds-yankodesign13.jpg",
-      "img": "https://www.yankodesign.com/images/design_news/2020/02/auto-draft/uncomfortable_designs-ds-yankodesign13.jpg"
+      "status": "nuevo",
+      "description": "El diseño es tan malo que querrás quejarte (o en ingles, whine!). Pero al menos no hay derrames.",
+      "image": "https://www.yankodesign.com/images/design_news/2020/02/auto-draft/uncomfortable_designs-ds-yankodesign13.jpg"
     }
+  ]   
   ];
 
 router.get('/', (req, res) => {
