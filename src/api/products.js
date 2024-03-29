4404843eeaee795fe8ad98290c1a4bef151f992f
Hack3r-han <hannahphil27@gmail.com>
2024-03-29 16:10:48 +0100

    Get one product with ID

--- src/api/products.js
+++ src/api/products.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json([
+    
+const productsData = {
+      "products":[
     {
-      "id_product": 1,
+      "id": 1,
       "name": "Cowboy Boots",
       "description": "Pair these outlaw boots with your favorite jeans, a worn-in hat, and a leather jacket for an ensemble that exudes Western charisma. Whether you're in a honky-tonk or blazing your own trail, these boots are your partner in crime. Size 9.",
       "price": 35,
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
       "status": "used"
     },
     {
-      "id_product": 2,
+      "id": 2,
       "name": "Woolen Jumper",
       "description": "Cozy and stylish woolen jumper to keep you warm during chilly seasons. This garment seamlessly combines comfort and fashion, making it a must-have addition to your winter wardrobe. Size S.",
       "price": 15,
@@ -29,7 +29,7 @@ router.get('/', (req, res) => {
       "status": "new"
     },
     {
-      "id_product": 4,
+      "id": 4,
       "name": "One-Sheet Wonder",
       "description": "Introducing our revolutionary Quantum Absorbency Toilet Roll—an unparalleled breakthrough in bathroom innovation. With an extreme absorbing power that defies expectations, this toilet roll is engineered to redefine your bathroom experience.",
       "price": 8,
@@ -37,7 +37,7 @@ router.get('/', (req, res) => {
       "status": "new"
     },
     {
-      "id_product": 5,
+      "id": 5,
       "name": "Knitted Jumper",
       "description": "Knitted sweater made from high-quality 100% merino wool. Please note that the mushrooms are not edible. Size M.",
       "price": 20,
@@ -45,7 +45,7 @@ router.get('/', (req, res) => {
       "status": "used"
     },
     {
-      "id_product": 6,
+      "id": 6,
       "name": "Dali's Espadrilles",
       "description": "Once graced by the surreal master himself, Salvador Dali. More than mere footwear, these espadrilles offer a unique portal into the fantastical landscapes that fueled the artist's creativity. The canvas of your mind will become a playground of surreal ideas. Size 9.",
       "price": 100,
@@ -53,7 +53,7 @@ router.get('/', (req, res) => {
       "status": "new"
     },
     {
-      "id_product": 7,
+      "id": 7,
       "name": "Wearable Umbrella",
       "description": "Designed for those who refuse to let a sudden downpour dampen their style, this revolutionary accessory combines functionality with fashion. The hands-free design allows you to carry on with your day, whether you're navigating busy streets, juggling shopping bags, or answering calls-all while staying perfectly dry.",
       "price": 10,
@@ -61,7 +61,7 @@ router.get('/', (req, res) => {
       "status": "new"
     },
     {
-      "id_product": 8,
+      "id": 8,
       "name": "Speaker",
       "description": "Immerse yourself in a realm of unparalleled audio excellence with this amazing speaker. From deep, resonant bass to the finest trebles, experience audio clarity like never before.",
       "price": 30,
@@ -69,7 +69,7 @@ router.get('/', (req, res) => {
       "status": "used"
     },
     {
-      "id_product": 9,
+      "id": 9,
       "name": "Fried Confidence Socks",
       "description": "Step confidently into a world of self-assured style with these socks. Forget about walking on eggshells and embrace every step with boldness. Crafted for those who exude confidence, these socks boast a fried egg design.",
       "price": 8,
@@ -77,7 +77,7 @@ router.get('/', (req, res) => {
       "status": "new"
     },
     {
-      "id_product": 10,
+      "id": 10,
       "name": "Koh-i-Noor",
       "description": "Behold the Koh-i-Noor Diamond, a treasure that embodies a rich tapestry of history, power, and unparalleled beauty. This world-renowned diamond, with a history steeped in mystery and grandeur, has captivated the imagination of rulers, conquerors, and admirers for centuries.",
       "price": 100000000,
@@ -85,7 +85,7 @@ router.get('/', (req, res) => {
       "status": "new"
     },
     {
-      "id_product": 11,
+      "id": 11,
       "name": "The Golden Fleece",
       "description": "An embodiment of the suns golden rays.  This shimmering fleece, once guarded by the mighty ram Chrysomallos, beckons those with the heart of a hero to embark on a journey of unparalleled significance.",
       "price": 90,
@@ -93,7 +93,7 @@ router.get('/', (req, res) => {
       "status": "used"
     },
     {
-      "id_product": 12,
+      "id": 12,
       "name": "WC Fish n Flush",
       "price": 99.90,
       "status": "new",
@@ -101,7 +101,7 @@ router.get('/', (req, res) => {
       "image": "https://assets.newatlas.com/dims4/default/32dc20a/2147483647/strip/true/crop/530x647+0+0/resize/530x647!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2Farchive%2F6076_29080652716.jpg"
     },
     {
-      "id_product": 13,
+      "id": 13,
       "name": "Cubed Watermelon",
       "price": 29.99,
       "status": "new",
@@ -109,14 +109,30 @@ router.get('/', (req, res) => {
       "image": "https://cdn.thisiswhyimbroke.com/images/cubed-watermelon-mold-640x533.jpg"
     },
     {
-      "id_product": 14,
+      "id": 14,
       "name": "Uncomfortable Wine Glass",
       "price": 200,
       "status": "new",
       "description": "Particularly uncomfortable glass, which allows you to smell your wine better when drinking it. Difficult to wash. In very good condition.",
       "image": "https://www.yankodesign.com/images/design_news/2020/02/auto-draft/uncomfortable_designs-ds-yankodesign13.jpg"
     }
-  ]);
+  ]
+};
+
+router.get('/', (req, res) => {
+  res.json(productsData);
+});
+
+router.get('/:productId', (req, res) => {
+  const productId = parseInt(req.params.productId);
+
+  const product = productsData.products.find(product => product.id === productId);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: "Product not found" });
+  }
 });
 
 module.exports = router;
